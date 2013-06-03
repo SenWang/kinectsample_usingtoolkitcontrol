e7a6b582dfbd05d15262a6a0a8b19f7263e39c52
Sen Wang <moli.wang@gmail.com>
2013-06-03 17:21:49 +0800

    add .NET assembly ( Kinect & Toolkit ) and using KinectSensorChooserUI element

---
+++ kinectsample_usinginteractioncontrol/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Kinect.Toolkit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kinectsample_usinginteractioncontrol
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        KinectSensorChooser sensorChooser;
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            sensorChooser = new KinectSensorChooser(); 
+            sensorChooser.KinectChanged +=sensorChooser_KinectChanged;
+            sensorChooserUI.KinectSensorChooser = sensorChooser;
+            sensorChooser.Start(); 
+        }
+
+        void sensorChooser_KinectChanged(object sender, KinectChangedEventArgs e)
+        {
+            if(e.NewSensor == null)
+                Title = "找不到 Kinect感應器";
+            else
+                Title = "感應器狀態 : " + e.NewSensor.Status ;
+                    
+        }
+    }
+}
